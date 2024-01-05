1447558d442a9b1f20aaec60a9266857beac48cc
sayyadjeba <155734907+sayyadjeba@users.noreply.github.com>
2024-01-05 17:26:01 +0530

    Arithmetic.java

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,1 +1,35 @@
+import java.util.Scanner;
+
+ public class ArithmeticOperation {
+
+ public static void main(String[]
+
+args) {
+
+Scanner sc = new Scanner(System.in );
+
+System.out.println("Enter numbers :");
+int num1 = sc.nextInt();
+
+int num2 = sc.nextInt();
+
+ int sum num1 + num2;
+
+int sub = num1 - num2;
+
+int mult = num1 * num2;
+
+ int div = num1 / num2;
+System.out.println("additon is: + sum);
+ System.out.println("subtraction is: + sub);
+
+ System.out.println("multiplication is: " + mult);
+
+ System.out.println("division is: "
+
++ div);
+
+ }
+
+ }
 
